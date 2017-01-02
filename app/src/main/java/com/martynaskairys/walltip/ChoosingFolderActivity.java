d9ad4eec9848aaefc8adfafb8022966b19ec3036
martynaskairys <martynaskairys@gmail.com>
2017-01-02 06:52:20 +0200

    Delete drawables that are not used or not important

--- app/src/main/java/com/martynaskairys/walltip/ChoosingFolderActivity.java
+++ app/src/main/java/com/martynaskairys/walltip/ChoosingFolderActivity.java
@@ -37,9 +37,9 @@ public class ChoosingFolderActivity extends AppCompatActivity {
 	private int[] dailyInspirationThumbIds = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4,
 			R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9};
 	private int[] healthyLifeThumbIds = {R.drawable.b1,R.drawable.b2, R.drawable.b3,R.drawable.b4
-			,R.drawable.b5,R.drawable.b6,R.drawable.b7,R.drawable.b8,R.drawable.b9,R.drawable.b10,R.drawable.b11};
+			,R.drawable.b5,R.drawable.b6,R.drawable.b7,R.drawable.b8,R.drawable.b9};
 	private int[] bePositiveThumbIds = {R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5, R.drawable.c6,
-			R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.c10, R.drawable.c11, R.drawable.c12};
+			R.drawable.c7, R.drawable.c8, R.drawable.c9};
 
 	private UserTracker userTracker = new UserTrackerImpl();
 
