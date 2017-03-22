f9fdf75af74628fc81539607a008219e14cdb8c5
martynaskairys <martynaskairys@gmail.com>
2017-03-22 16:08:42 +0200

    Remove setHomeAsUpIndicator for PictureActivity

--- app/src/main/java/com/martynaskairys/walltip/PictureActivity.java
+++ app/src/main/java/com/martynaskairys/walltip/PictureActivity.java
@@ -1,9 +1,6 @@
 package com.martynaskairys.walltip;
 
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -24,12 +21,7 @@ public class PictureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
-
         userTracker.reportInPictureActivityOnCreate();
-
-        final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_white_24dp);
-        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.primary), PorterDuff.Mode.SRC_ATOP);
-        getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
         int imageInt = getIntent().getIntExtra(IMAGE_INT, R.drawable.mok);
         ImageView imageView = (ImageView)findViewById(R.id.imageView);
